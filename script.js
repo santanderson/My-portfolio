e80c845e79cc9683d120404db6019018366ca1d7
Anderson Ryan <santoryan1408@gmail.com>
2022-01-23 16:46:41 -0200

    Versão final

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener('click', contato);
 
 function contato(event){
     const element = event.target;
-    const button = document.querySelector('.contact');
     const aside = document.querySelector('aside');
 
     if(element.classList == 'contact'){aside.style = 'display: flex';}
